1d1ac7c2a9260e89f13f4cdf5bf7ab54b41160be
Richarde Lira <richarde-ips@outlook.com>
2022-10-25 14:08:23 -0300

    Tensor Dataset and ObjectDetector

---
+++ Train.py
@@ -0,0 +1,21 @@
+# import the necessary packages
+from pyimagesearch.bbox_regressor import ObjectDetector
+from pyimagesearch.custom_tensor_dataset import CustomTensorDataset
+from Config import Config 
+from sklearn.preprocessing import LabelEncoder
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torch.nn import CrossEntropyLoss
+from torch.nn import MSELoss
+from torch.optim import Adam
+from torchvision.models import resnet50
+from sklearn.model_selection import train_test_split
+from imutils import paths
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle
+import torch
+import time
+import cv2
+import os
